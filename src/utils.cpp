81866c1cd7e31498a2b51f2ea37c310096c2ca7f
Jarem Vargas <jmvargasc@unjbg.edu.pe>
2025-10-09 21:07:02 -0500

    utils.cpp - Implementación using namespace

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,17 +1,18 @@
 #include "utils.hpp"
 #include <iostream>
 #include <sstream>
+using namespace std;
 
-std::vector<std::string> split(const std::string& str, char delimiter) {
-    std::vector<std::string> tokens;
-    std::stringstream ss(str);
-    std::string item;
-    while (std::getline(ss, item, delimiter)) {
+vector<string> split(const string& str, char delimiter) {
+    vector<string> tokens;
+    stringstream ss(str);
+    string item;
+    while (getline(ss, item, delimiter)) {
         tokens.push_back(item);
     }
     return tokens;
 }
 
-void print_error(const std::string& msg) {
-    std::cerr << "Error: " << msg << std::endl;
+void print_error(const string& msg) {
+    cerr << "Error: " << msg << endl;
 }
